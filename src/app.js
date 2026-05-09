438f2912569840cbf1c265dde93420e41841d699
md-sadidtahsin <sadidtahsin@hotmail.com>
2026-05-09 17:48:54 +0600

    fixing the code

--- src/app.js
+++ src/app.js
@@ -1,22 +1,38 @@
 const express = require('express');
+const dotenv = require('dotenv');
 const jwt = require("jsonwebtoken");
 const app = express();
 
+dotenv.config();
 
 app.disable("x-powered-by");
 app.use(express.json());
 
 // ❌ Bug: Hardcoded secret (bad practice)
-const SECRET_KEY = "12345-plaintext-secret"; 
+// const SECRET_KEY = "12345-plaintext-secret"; 
+
+// ✅ Fix: Use environment variable for secret key
+const SECRET_KEY = process.env.SECRET_KEY;
 
 app.get('/', (req, res) => {
   res.send("Hello World");
 });
 
 // ❌ Vulnerability: Using eval (dangerous)
+// app.get('/eval', (req, res) => {
+//   const code = req.query.code;
+//   res.send(eval(code)); // Sonar will flag this
+// });
+
+
+// ✅ Fix: Replace eval with a safe alternative
 app.get('/eval', (req, res) => {
   const code = req.query.code;
-  res.send(eval(code)); // Sonar will flag this
+    if (code === "2+2") {
+        res.status(200).send("4");
+    } else {
+        res.status(400).send("Invalid code");
+    }
 });
 
 app.post("/login", (req, res) => {
